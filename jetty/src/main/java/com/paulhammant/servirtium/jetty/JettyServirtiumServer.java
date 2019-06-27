b7c36c02a8c493f124a8f37b019eb320be252ea0
Paul Hammant <paul@hammant.org>
2019-06-27 20:36:20 +0100

    url not recorded properly

--- jetty/src/main/java/com/paulhammant/servirtium/jetty/JettyServirtiumServer.java
+++ jetty/src/main/java/com/paulhammant/servirtium/jetty/JettyServirtiumServer.java
@@ -54,7 +54,7 @@ public class JettyServirtiumServer extends ServirtiumServer {
         String method = request.getMethod();
 
         String url = request.getRequestURL().toString();
-        final String uri = request.getRequestURI();
+        final String uri = ((Request) request).getOriginalURI();
 
         url = (url.startsWith("http://") || url.startsWith("https://"))
                 ? url : "http://" + request.getRemoteHost() + ":" + request.getRemotePort() + uri;
